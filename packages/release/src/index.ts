cf83c95a238886f306044b64612c2e924d2f5ee1
vyron <fiee_vyron@163.com>
2026-06-09 12:53:38 +0800

    feat(release): add type definitions

--- packages/release/src/index.ts
+++ packages/release/src/index.ts
@@ -1,13 +1,4 @@
-export interface ReleaseOptions {
-  cwd?: string
-  dryRun?: boolean
-  skipTests?: boolean
-  skipPublish?: boolean
-  skipPush?: boolean
-  releaseAs?: string
-  all?: boolean
-  package?: string
-}
+export type { ReleaseOptions, PkgInfo, ReleaseStep, CommitInfo, ReleaseType } from './types'
 
 export async function release(options: ReleaseOptions = {}): Promise<void> {
   // TODO: 实现
---
+++ packages/release/src/types.ts
@@ -0,0 +1,33 @@
+// 发布选项
+export interface ReleaseOptions {
+  cwd?: string
+  dryRun?: boolean
+  skipTests?: boolean
+  skipPublish?: boolean
+  skipPush?: boolean
+  releaseAs?: string
+  all?: boolean
+  package?: string
+}
+
+// package.json 信息
+export interface PkgInfo {
+  name: string
+  version: string
+  path: string
+}
+
+// 发布步骤
+export interface ReleaseStep {
+  name: string
+  run: () => Promise<void>
+}
+
+// Git 提交信息
+export interface CommitInfo {
+  message: string
+  files?: string[]
+}
+
+// 版本类型
+export type ReleaseType = 'patch' | 'minor' | 'major' | 'custom'
